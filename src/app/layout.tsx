7cfc9356a1ba73d50aa87cd573683668754c4689
ishworX <git.ishwor@gmail.com>
2024-08-18 00:12:03 -0600

    Installing the dependencies

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "New Taste Of India",
+  description: "New taste of India restaurant, Indian, Indo chinese, Nepali cuisine.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
