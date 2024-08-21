68fc8dcb0191a1d86c3da41dc357805a6e593a61
ishworX <git.ishwor@gmail.com>
2024-08-20 23:03:54 -0600

    nav test?

--- src/app/components/NavBar/Navbar.jsx
+++ src/app/components/NavBar/Navbar.jsx
@@ -4,6 +4,32 @@ import './Navbar.css'
 
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <nav
+            id="navbar"
+            className={`navbar order-last order-lg-0 ${open ? 'navbar-mobile' : undefined
+                }`}
+        >
+            <ul>
+                {navList.map(nav => (
+                    <li key={nav.id}>
+                        <a
+                            className={`nav-link scrollto ${nav.active ? 'active' : undefined
+                                }`}
+                            onClick={() => handleScrollTo(nav.target)}
+                        >
+                            {nav.name === 'Home' ? (
+                                <i className="bi bi-house-door-fill"></i>
+                            ) : (
+                                nav.name
+                            )}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <i
+                className="bi bi-list mobile-nav-toggle"
+                onClick={handleToggleMenu}
+            ></i>
+        </nav>
     )
 }
