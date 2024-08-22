4fa22aac38a50ad29fb0f4c5d6c63c3b050abe1f
ishworX <git.ishwor@gmail.com>
2024-08-21 23:28:49 -0600

    working on the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "./sections/Hero/Hero";
 
 export default function Home() {
   return (
-    <h1> </h1>
+    <>
+      <Hero />
+    </>
+    
   );
 }
