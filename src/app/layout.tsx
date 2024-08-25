d8d3512405bb68921783141ef0b065be026b0246
ishworX <git.ishwor@gmail.com>
2024-08-25 15:28:13 -0600

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ import "./globals.css";
 // Importing custimed component
 import TopBar from './components/TopBar/TopBar';
 import Header from './components/Header/Header';
+import BackToTopBtn from './components/Button/BackToTopBtn';
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
@@ -38,11 +39,8 @@ export default function RootLayout({
         <TopBar />
         <Header />
         {children}
+        <BackToTopBtn />
         </body>
-      {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh950GNyZPhc TNXj1NW7RuBCsyN/00jlpcV8Qyq46cDfL"
-        crossOrigin="anonymous">
-      </script> */}
     </html>
   );
 }
